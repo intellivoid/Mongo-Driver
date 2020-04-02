b3f62c7d9c58ce6eeb44fcc2e3221f9244277fa2
netkas <netkas@intellivoid.info>
2020-04-02 15:52:13 -0300

    Updated Operation

--- src/Operation/Executable.php
+++ src/Operation/Executable.php
@@ -1,25 +1,25 @@
 <?php
 
 
-namespace MongoDB\Operation;
+    namespace MongoDB\Operation;
 
-use MongoDB\Driver\Server;
+    use MongoDB\Driver\Server;
 
-/**
- * Executable interface for operation classes.
- *
- * This interface is reserved for internal use until PHPC-378 is implemented,
- * since execute() should ultimately be changed to use ServerInterface.
- *
- * @internal
- */
-interface Executable
-{
     /**
-     * Execute the operation.
+     * Executable interface for operation classes.
      *
-     * @param Server $server
-     * @return mixed
+     * This interface is reserved for internal use until PHPC-378 is implemented,
+     * since execute() should ultimately be changed to use ServerInterface.
+     *
+     * @internal
      */
-    public function execute(Server $server);
-}
+    interface Executable
+    {
+        /**
+         * Execute the operation.
+         *
+         * @param Server $server
+         * @return mixed
+         */
+        public function execute(Server $server);
+    }
